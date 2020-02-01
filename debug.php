363a82810174603a1d339706977e18cd7ef94740
Česlav Przywara <ceslav@przywara.cz>
2020-02-01 16:29:42 +0100

    Modernize Debug plugin

--- debug.php
+++ debug.php
@@ -2,36 +2,39 @@
 /**
  * Plugin Name: Debug
  * Description: Handy debugging functions.
- * Version: 2.0.1
+ * Version: 2.0.2
  * Author: Česlav Przywara <cp@bluechip>
- * Author URI: http://www.bluechip.at
+ * Author URI: https://www.bluechip.at
  */
 
 namespace Debug;
 
 /**
- * Print all passed arguments to error log, if WP_DEBUG is true: arrays and
- * objects are printed via print_r, any other type is printed via var_export
- * @param mixed $	Variable(s) you want to print to error log.
+ * Print all arguments to error log if WP_DEBUG is true.
+ *
+ * Arrays and objects are printed via print_r, any other type is printed via var_export.
+ *
+ * @param mixed $ Variable(s) you want to print to error log.
  */
 function dump()
 {
     if (true === WP_DEBUG) {
-        foreach (func_get_args() as $arg) {
-            error_log((is_array($arg) || is_object($arg)) ? print_r($arg, true) : var_export($arg, true));
+        foreach (\func_get_args() as $arg) {
+            \error_log((\is_array($arg) || \is_object($arg)) ? \print_r($arg, true) : \var_export($arg, true));
         }
     }
 }
 
 
 /**
- * Print backtrace to error log, if WP_DEBUG is true.
+ * Print backtrace to error log if WP_DEBUG is true.
+ *
  * @link http://php.net/manual/en/function.debug-print-backtrace.php#92542
  */
 function backtrace()
 {
     if (true === WP_DEBUG) {
         $e = new \Exception();
-        error_log(PHP_EOL . $e->getTraceAsString());
+        \error_log(PHP_EOL . $e->getTraceAsString());
     }
 }
